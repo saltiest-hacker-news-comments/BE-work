619d1c34bb48480760bcb12332b418dcac1fde80
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-09-22 18:15:00 -0400

    more fleshed out added auth and routes

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+
+///sanity check
+server.get('/', () => {
+    res.send('working in the servvver')
+
+})
+
+
+
+module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,48 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/salt.db3'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg', // install this package
+    connection: process.env.DATABASE_URL, // heroku sets this env variable
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+
+
+
+ 
 
 };
